7ab8f0c5f310246f79d1b7d747967d539be60623
HuwGames Studio <siliwang.huw@gmail.com>
2016-01-22 09:52:28 -0800

    Update ReturnButton.cs

--- Assets/Scripts/ReturnButton.cs
+++ Assets/Scripts/ReturnButton.cs
@@ -5,26 +5,26 @@ using System.Collections;
 public class ReturnButton : MonoBehaviour
 {
 	public static ReturnButton _instance;
-    private GameController gameController;
+	private GameController gameController;
 	public Text restartWait;
 	public bool REstart = false;
 	float RETimer = 10f;
 	float s = 10f;
-
+	
 	void Awake(){
 		_instance=this;
 	}
 
 
-    void Start()
-    {
-        gameController = GameObject.FindObjectOfType<GameController>();
+    	void Start()
+    	{
+        	gameController = GameObject.FindObjectOfType<GameController>();
 //		KTPlay.ShowInterstitialNotification();
-    }
+    	}
 
-    // Update is called once per frame
-    void Update()
-    {
+    	// Update is called once per frame
+    	void Update()
+    	{
 		if(REstart){
 			RETimer -= Time.deltaTime;
 			s = (int)RETimer;
@@ -32,13 +32,11 @@ public class ReturnButton : MonoBehaviour
 				restartWait.text = "Try Again (" + s + ")";
 			}
 		}
-
-
-    }
-    void OnMouseDown()
-    {
-        gameController.ReturnStartUI();
-    }
+    	}
+    	void OnMouseDown()
+    	{
+        	gameController.ReturnStartUI();
+    	}
 
 	public void ReadyToRestart(){
 		REstart = true;
